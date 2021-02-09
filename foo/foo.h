5906337312993d047292c3d5b9d54bd337465402
Daniel Zhou <danzhou1810@gmail.com>
2021-02-08 22:09:56 -0500

    fix(job): use correct assumption for bound-check

--- foo/foo.h
+++ foo/foo.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#ifndef ulong
 typedef unsigned long ulong;
+#endif
+
+#ifndef uint
 typedef unsigned int uint;
+#endif
 
 unsigned foo(unsigned* arr, unsigned* indices);
 ulong foo_decompiled(uint *param_1,uint *param_2);
